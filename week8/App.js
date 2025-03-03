5698839297a34f766f020be4cb1bff4de9668808
charlesdave27 <godseeker027@gmail.com>
2025-03-03 18:39:51 +0800

    App.js

--- week8/App.js
+++ week8/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, ScrollView, Image} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import Icon from 'react-native-vector-icons/Ionicons';
+
 
 export default function App() {
   const [page, setPage] = useState(1);
@@ -20,51 +23,72 @@ export default function App() {
     <ScrollView contentContainerStyle={styles.container}>
       {page === 1 && (
         <View style={styles.page}>
-          <Image style={styles.image} source={{uri: 'https://th.bing.com/th/id/OIP.Hel1rUkKSwgqt29tie7v0AAAAA?w=340&h=270&rs=1&pid=ImgDetMain'}}/>
-          <Text style={styles.header}>Charles Dave Dela Cruz</Text>
+          <Image style={styles.image} source={require('./assets/profile.jpeg')}/>
+          <Text style={styles.header }>Charles Dave Dela Cruz</Text>
           <Text style={styles.sectionTitle}>Contact Information</Text>
-          <Text style={styles.text}>Email: Charlesadave28@gmail.com</Text>
-          <Text style={styles.text}>Phone: 09457801364</Text>
-          <Text style={styles.text}>Address: 12 T Marcelo St, Dalandanan Val.</Text>
-
+          
+        <View style= {styles.contacts}>
+          <View style= {styles.contact}>
+            <Ionicons name="mail" size={20} color="white" 
+            style={styles.icon} />
+            <Text style={styles.text}>Charlesadave28@gmail.com</Text>
+          </View> 
+          <View style= {styles.contact}>
+            <Ionicons name="call" size={20} color="green" 
+            style={styles.icon} />
+            <Text style={styles.text}> 09457801364</Text>
+          </View> 
+          <View style= {styles.contact}>
+            <Ionicons name="location" size={20} color="red" 
+            style={styles.icon} />
+            <Text style={styles.text}> 12 T Marcelo St, Dalandanan Val.
+            </Text>
+          </View> 
+        </View>
           <TouchableOpacity style={styles.button} onPress={handleNextPage}>
-            <Text style={styles.buttonText}>Next Page</Text>
+            <Icon name="arrow-forward" size={30} color="yellowgreen" />
           </TouchableOpacity>
         </View>
       )}
 
       {page === 2 && (
         <View style={styles.page}>
-          <Text style={styles.sectionTitle}>About me</Text>
-          <Text style={styles.text}> Hello I'm Charles Dave Dela Cruz, an aspiring IT professional passionate about technology, software development, and problem-solving. Currently pursuing my studies in Information Technology, I want to specialize in web development. I am always eager to learn new technologies,  and enhance my skills in full stack web development I'm interest in how computers work and believe technology is the way to the advancement of society in the future</Text>
-
+          <Text style={styles.header}>About me</Text>
+          <Text style={{fontSize: 18, color: 'yellow', textAlign: 'center'}}>    Hello I'm Charles Dave Dela Cruz, an aspiring IT professional passionate about technology, software development, and problem-solving. Currently pursuing my studies in Information Technology, I want to specialize in web development. I am always eager to learn new technologies,  and enhance my skills in full stack web development I'm interest in how computers work and believe technology is the way to the advancement of society in the future</Text>
+          <View style= {styles.contact}>
           <TouchableOpacity style={styles.button} onPress={handlePrevPage}>
-            <Text style={styles.buttonText}>Previous Page</Text>
+            <Icon name="arrow-back" size={30} color="yellowgreen" />
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={handleNextPage}>
-            <Text style={styles.buttonText}>Next Page</Text>
+            <Icon name="arrow-forward" size={30} color="yellowgreen" />
           </TouchableOpacity>
+          </View>
         </View>
       )}
 
       {page === 3 && (
         <View style={styles.page}>
           <Text style={styles.header}>Education</Text>
-          <Text style={styles.sectionTitle}>Elementary</Text>
-          <Text style={styles.text}>Antonio Regidor (2007 - 2013)</Text>
-          <Text style={styles.sectionTitle}>High School</Text>
-          <Text style={styles.text}>Dalandanan National High School (2013 - 2016)</Text>
-          <Text style={styles.sectionTitle}> Senior High School</Text>
-          <Text style={styles.text}>Dalandanan National High School (2016 - 2019)</Text>
-          <Text style={styles.sectionTitle}> College</Text>
-          <Text style={styles.text}>Global Reciprocal College (2022 - )</Text>
-
+          <View style={{alignContent: 'flex-start'}}>
+            <Text style={styles.sectionTitle}>Elementary</Text>
+            <Text style={[styles.text]}>  Antonio Regidor <Text style={{color: 'grey'}}>(2007 - 2013) </Text></Text>
+            <Text style={styles.sectionTitle}>High School</Text>
+            <Text style={styles.text}>  Dalandanan National High School</Text>
+            <Text style={{color: 'grey', paddingBottom: 10}}>      (2013 - 2017)</Text>
+            <Text style={styles.sectionTitle}>Senior High School</Text>
+            <Text style={styles.text}>  Dalandanan National High School</Text>
+            <Text style={{color: 'grey', paddingBottom: 10}}>      (2016 - 2019)</Text>
+            <Text style={styles.sectionTitle}>College</Text>
+            <Text style={styles.text}>  Global Reciprocal College</Text><Text style={{color: 'grey'}}>     (2022 - present)</Text>
+          </View>
+          <View style= {styles.contact}>
           <TouchableOpacity style={styles.button} onPress={handlePrevPage}>
-            <Text style={styles.buttonText}>Previous Page</Text>
+            <Icon name="arrow-back" size={30} color="yellowgreen" />
           </TouchableOpacity>
           <TouchableOpacity style={styles.button} onPress={handleNextPage}>
-            <Text style={styles.buttonText}>Next Page</Text>
+            <Icon name="arrow-forward" size={30} color="yellowgreen" />
           </TouchableOpacity>
+          </View>
         </View>
       )}
 
@@ -75,9 +99,8 @@ export default function App() {
           <Text style={styles.text}>Inventory System</Text>
           <Text style={styles.sectionTitle}>DBMS</Text>
           <Text style={styles.text}>Inventory System Database</Text>
-
           <TouchableOpacity style={styles.button} onPress={handlePrevPage}>
-            <Text style={styles.buttonText}>Previous Page</Text>
+            <Icon name="arrow-back" size={30} color="yellowgreen" />
           </TouchableOpacity>
         </View>
       )}
@@ -87,50 +110,54 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flexGrow: 1,
-    padding: 20,
-   backgroundColor: 'black',
+  flexGrow: 1,
+  padding: 20,
+   backgroundColor: '#1e1e1e',
   },
   page: {
-    flexDirection: 'center',
-    alignItems: 'center',
-    backgroundColor: 'black'
+    alignItems: 'center'
   },
   header: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginBottom: 10,
+    marginBottom: 40,
     color: '#87ceeb'
   },
   image: {
     width: 100,
     height: 100,
     borderRadius: 60,
+    alignItems: 'center'
   },
   sectionTitle: {
     fontSize: 20,
     fontWeight: 'bold',
-    marginTop: 15,
-    marginBottom: 40,
-    color: '#008080'
+    marginBottom: 10,
+    color: '#008080',
   },
   text: {
     fontSize: 16,
-    marginLeft: 10,
     color: 'white',
-    marginBottom: 20
+    marginBottom: 10
     
   },
   button: {
     marginTop: 20,
-    backgroundColor: 'grey',
-    paddingVertical: 10,
-    paddingHorizontal: 30,
-    borderRadius: 5,
-    alignItems: 'center',
+    flexDirection: 'row',
+    width: 50,
+    justifyContent: 'center',
   },
-  buttonText: {
-    color: 'white',
-    fontSize: 18,
+ 
+  contacts: {
+    flexDirection: 'column',
+    marginVertical: 20
   },
+  contact:{
+    flexDirection: 'row',
+     marginVertical: 1
+  },
+  icon: {
+    paddingRight: 10,
+  },
+
 });
